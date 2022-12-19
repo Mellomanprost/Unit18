5775b741308841cb5729e44901b2515d951a45cb
Mellomanprost <anoppa@mail.ru>
2022-12-19 21:58:39 +0300

    Added BuilderExample

---
+++ Unit18/Unit18.1/BuilderExample/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderExample
+{
+    class Client
+    {
+        static void Main()
+        {
+            // инициализация строителя
+            IBuilder builder = new ProductBuilder();
+            // Создаем менеджера, который будет рулить процессом
+            BuildManager buildManager = new BuildManager(builder);
+
+            //  Стартуем и получаем результат
+            buildManager.StartBuildingProcess();
+            Product product = builder.GetBuildingResult();
+        }
+    }
+}
--- Unit18/Unit18.1/BuilderRealExample/Properties/AssemblyInfo.cs
+++ Unit18/Unit18.1/BuilderRealExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Unit18")]
+[assembly: AssemblyTitle("BuilderRealExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unit18")]
+[assembly: AssemblyProduct("BuilderRealExample")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("da736896-3e88-4e83-8088-685b3780fc27")]
+[assembly: Guid("5f5647b9-3ff9-4649-883f-92a8a2b2a949")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
